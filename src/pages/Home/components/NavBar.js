e5ea9c98a4fb9bec8175e85de2e7b403b012b116
Lucas Nhimi <lucaslonguinhos@gmail.com>
2020-05-03 16:02:58 -0300

    Custom NavBar component

--- src/pages/Home/components/NavBar.js
+++ src/pages/Home/components/NavBar.js
@@ -1,10 +1,48 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button';
+import ListSubheader from '@material-ui/core/ListSubheader';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    padding: theme.spacing(2),
+    width: 275
+  },
+  button: {
+    width: '100%'
+  }
+}));
+
+const tags = [
+  { id: 1, name: 'reactjs' },
+  { id: 2, name: 'javascript' },
+  { id: 3, name: 'dotnet' },
+  { id: 4, name: 'php' },
+  { id: 5, name: 'materialdesign' },
+  { id: 6, name: 'webdev' },
+]
 
 function NavBar() {
+  const classes = useStyles();
+
   return (
-    <div>
-      NavBar
-    </div>
+    <Paper className={classes.root}>
+      <Button variant="outlined" color="secondary" className={classes.button}>Registrar Gratis</Button>
+      <ListSubheader>{`Tags em alta`}</ListSubheader>
+      {
+        tags.map((item) => (
+          <ListItem dense button key={`item-${item.id}-${item.name}`}>
+            <ListItemText primary={`#${item.name}`} />
+          </ListItem>
+        ))
+      }
+      <ListItem button>
+        Exibir mais Tags
+      </ListItem>   
+    </Paper>
   )
 }
 
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -5,6 +5,8 @@ import { makeStyles } from '@material-ui/core/styles';
 import Header from './components/Header';
 import Feed from './components/Feed';
 import NavBar from './components/NavBar';
+import Container from '@material-ui/core/Container';
+import Box from '@material-ui/core/Box';
 
 const useStyles = makeStyles({
   root: {
@@ -13,9 +15,10 @@ const useStyles = makeStyles({
   },
   main: {
     height: '100vh',
-    display: 'flex',
-    width: '1200px',
-    margin: '0 auto',
+    padding: 24
+  },
+  toolbar: {
+    minHeight: 64,
   }
 })
 
@@ -25,10 +28,14 @@ function Home() {
   return (
     <div className={classes.root}>      
       <Header />
-      <div className="toolbar"></div>
+      <div className={classes.toolbar}></div>
       <main className={classes.main}>
-        <NavBar />
-        <Feed />        
+        <Container maxWidth="lg">
+          <Box display="flex">
+            <NavBar />
+            <Feed />
+          </Box>
+        </Container>
       </main>
     </div>
   )
