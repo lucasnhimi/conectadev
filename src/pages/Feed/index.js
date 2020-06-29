4b70ac6156d07b6522065fa5549b8f9aff607d41
Lucas Nhimi <lucaslonguinhos@gmail.com>
2020-06-29 14:27:07 -0300

    Layout Responsive

--- src/pages/Feed/index.js
+++ src/pages/Feed/index.js
@@ -1,6 +1,7 @@
 import React, { useCallback, useEffect, useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
+import Hidden from '@material-ui/core/Hidden';
 import Box from '@material-ui/core/Box';
 
 import axios from '../../utils/axios';
@@ -27,7 +28,9 @@ function Feed() {
   return (
     <Container maxWidth="lg">
       <Box display="flex">
-        <NavBar />
+        <Hidden smDown>
+          <NavBar />
+        </Hidden>
         <div className={classes.root}>
           {posts.map((post) => (
             <PostCard key={post.id} post={post} />
